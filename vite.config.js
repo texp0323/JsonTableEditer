d1405923a7dd51735b789477457ec5ca5cbf2dc5
Hyeonjae Ju <hyeonjae.ju@playlinks.com>
2025-05-16 17:52:13 +0900

    Update: 배포

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import { defineConfig } from 'vite';
 export default defineConfig({
     base: '/JsonTableEditer/', // GitHub 저장소 이름을 여기에 입력하세요. 예: '/json-editor-app/'
     build: {
-        outDir: 'dist' // 빌드 결과물이 생성될 폴더 (기본값)
+        outDir: 'docs' // 빌드 결과물이 생성될 폴더 (기본값)
     }
 });
